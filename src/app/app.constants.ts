45cdf57b9d9e0575f8b6bbcea1f7191b5354df04
wesley <wesleybruno910@gmail.com>
2024-01-03 22:55:12 -0300

    fix uri

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
 // export const API_URI = 'http://ec2-3-85-159-127.compute-1.amazonaws.com';
 // export const API_URI = 'http://ec2-54-166-167-126.compute-1.amazonaws.com';
-export const API_URI = 'https://samurai-cars.onrender.com/';
+export const API_URI = 'https://samurai-cars.onrender.com';
